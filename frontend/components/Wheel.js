64033016d7dd58341533fa39233d32657d2cbf75
darrinlallen <darrin.lovard.allen@gmail.com>
2023-03-05 20:21:51 -0800

    clock directions

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,19 +1,62 @@
 import React from 'react'
+import {useState} from 'react'
 
 export default function Wheel(props) {
+  const [index, setindex] = useState(0)
+  let num =index
+  function clock (){
+   
+  
+    if (index == 5){
+    num = 0
+      setindex(0)
+    }
+    else {
+      if (index < 5){
+    setindex(index+1)}
+
+    num = {index}
+    
+    console.log(num)}
+
+    }
+  
+    function cclock (){
+     
+      if (index == 0 ){
+       num = 5
+        setindex(5)
+      }
+        else {
+      
+        setindex(index-1)
+        num = {index}
+     
+      console.log(num)}
+        
+      }
+  
+  let cl = "cog active"
+  let cl1 = "cog"
+  let cnt = 0
+  
+  
   return (
     <div id="wrapper">
       <div id="wheel">
-        <div className="cog active" style={{ "--i": 0 }}>B</div>
-        <div className="cog" style={{ "--i": 1 }}></div>
-        <div className="cog" style={{ "--i": 2 }}></div>
-        <div className="cog" style={{ "--i": 3 }}></div>
-        <div className="cog" style={{ "--i": 4 }}></div>
-        <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
+      { 
+        [0, 1, 2, 3, 4, 5].map((idx) => (
+        
+          <><div className="cog" style={{ "--i": idx }}></div><div key={idx} style={{ "--i": num }} className={`cog active${idx === num ? 'cog active' : ''}`}>B</div></>
+          
+            
+          ))
+       } 
       </div>
+    
       <div id="keypad">
-        <button id="counterClockwiseBtn" >Counter clockwise</button>
-        <button id="clockwiseBtn">Clockwise</button>
+        <button id="counterClockwiseBtn" onClick={cclock}>Counter clockwise</button>
+        <button id="clockwiseBtn" onClick={clock}>Clockwise</button>
       </div>
     </div>
   )
