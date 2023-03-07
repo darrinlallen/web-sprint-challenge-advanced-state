34a2e2be05e59257196de54c7883707387d4590f
darrinlallen <darrin.lovard.allen@gmail.com>
2023-03-06 18:36:37 -0800

    quiz

--- frontend/components/Quiz.js
+++ frontend/components/Quiz.js
@@ -1,6 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 
+let answer = ''
+let choiceme1 = 'SELECT'
+let choiceme2 = 'SELECT'
 export default function Quiz(props) {
+  const [selectme, setselectme] = useState('')
+  const [choice1, setchoice1] = useState('SELECT')
+  const [choice2, setchoice2] = useState('SELECT')
+  const[disabled, setdisabled] = useState(true)
+
+  function select1(){
+  setselectme('answer selected')
+  
+  setchoice1('SELECTED')
+  choiceme1 = choice1
+  setchoice2('select')
+  setdisabled(false)
+  }
+  function select2() {
+   setselectme('answer selected')
+    setchoice2('SELECTED')
+    choiceme2 = choice2
+    setchoice1('select')
+    }
+  
+
+  
   return (
     <div id="wrapper">
       {
@@ -8,27 +34,32 @@ export default function Quiz(props) {
         true ? (
           <>
             <h2>What is a closure?</h2>
-
+             
             <div id="quizAnswers">
-              <div className="answer selected">
+              <div className={`${choiceme1 === 'SELECTED' ? 'answer selected' : 'answer'}`}>
+
+          
                 A function
-                <button>
-                  SELECTED
+                <button onClick={select1}>
+                  {choice1}
                 </button>
               </div>
+                
+              <div className={`${choiceme2 === 'SELECTED' ? 'answer selected' : 'answer'}`}>
 
-              <div className="answer">
+          
                 An elephant
-                <button>
-                  Select
+                <button onClick={select2}>
+                  {choice2}
                 </button>
               </div>
             </div>
-
-            <button id="submitAnswerBtn">Submit answer</button>
+            
+            <button disabled = {disabled} id="submitAnswerBtn">Submit answer</button>
           </>
         ) : 'Loading next quiz...'
       }
+    
     </div>
   )
 }
